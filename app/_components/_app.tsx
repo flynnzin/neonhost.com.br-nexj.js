79fb438cdf71b5933929191c790863c6ed8bffc5
Cristopher Leonardo <116974699+flynnzin@users.noreply.github.com>
2025-02-16 23:15:33 -0300

    Create _app.tsx

---
+++ app/_components/_app.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.__ow = window.__ow || {};
+    window.__ow.organizationId = "25ce0692-2b59-4d7d-b682-3d0e74cc5269";
+    window.__ow.integration_name = "manual_settings";
+    window.__ow.product_name = "openwidget";
+
+    (function (n, t, c) {
+      function i(n) {
+        return e._h ? e._h.apply(null, n) : e._q.push(n);
+      }
+      var e = { _q: [], _h: null, _v: "2.0", on: function () { i(["on", c.call(arguments)]); }, 
+                once: function () { i(["once", c.call(arguments)]); }, 
+                off: function () { i(["off", c.call(arguments)]); }, 
+                get: function () { if (!e._h) throw new Error("[OpenWidget] You can't use getters before load."); return i(["get", c.call(arguments)]); }, 
+                call: function () { i(["call", c.call(arguments)]); }, 
+                init: function () { var n = t.createElement("script"); 
+                n.async = !0, n.type = "text/javascript", 
+                n.src = "https://cdn.openwidget.com/openwidget.js", 
+                t.head.appendChild(n); } 
+              }; 
+      !n.__ow.asyncInit && e.init(), n.OpenWidget = n.OpenWidget || e;
+    })(window, document, [].slice);
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
