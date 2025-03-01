c546fc0ae22f3281901feec1385fc149e62d1125
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-01 01:25:23 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -87,25 +87,6 @@ export default function Home() {
 				</div>
 			</section>
 
-			<script>
-  (function(d,t) {
-    var BASE_URL="https://app.chatwoot.com";
-    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=BASE_URL+"/packs/js/sdk.js";
-    g.defer = true;
-    g.async = true;
-    s.parentNode.insertBefore(g,s);
-    g.onload=function(){
-      window.chatwootSDK.run({
-        websiteToken: 'rw6cEHkmXEhGotozKLk7PyKJ',
-        baseUrl: BASE_URL
-      })
-    }
-  })(document,"script");
-</script>
-
-
-
 
 			<section className="pt-[15%]">
 				<div className="container">
