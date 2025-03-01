d4fcb6d7458f1ba59d95730b43f0d82005c1783c
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-01 01:28:02 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -197,19 +197,3 @@ export default async function RootLayout({
 		</html>
 	);
 }
-
-import { useEffect } from "react";
-import type { AppProps } from "next/app";
-
-function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "/chatwoot.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
