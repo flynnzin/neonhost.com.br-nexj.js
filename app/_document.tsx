e9204998c4b7cd2502b1d8c4114f80e32b853986
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-07 02:01:40 -0300

    Create _document.tsx

---
+++ app/_document.tsx
@@ -0,0 +1,49 @@
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          {/* Google Tag Manager (GTM) no Head - Carregamento assíncrono */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){
+                  w[l]=w[l]||[]; 
+                  w[l].push({
+                    'gtm.start': new Date().getTime(), 
+                    event: 'gtm.js'
+                  });
+                  var f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != 'dataLayer' ? '&l=' + l : '';
+                  j.async = true;
+                  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                  f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-NLK4RWTG');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          {/* Google Tag Manager no Body - Fallback para quando o JavaScript está desabilitado */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-NLK4RWTG"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
