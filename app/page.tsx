d4c7ad02a9c35715aa7630eb0c089b037898732c
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-06 23:00:31 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,21 @@ import {
 import { LoadingScreen } from "./_components/loading-screen";
 import Link from "next/link";
 
+import React from 'react';
+import Intercom from '@intercom/messenger-js-sdk';
+
+export default function Component() {
+  Intercom({
+    app_id: 'wo0haiuj',
+    user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
+    name: user.name, // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
+    email: user.email, // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
+    created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
+  });
+
+  return <div>Neon Host</div>;
+}
+
 const plans = [
 	{
 		icon: <Gamepad2 className="w-8 h-8 text-pink-500" />,
@@ -419,20 +434,4 @@ export default function Home() {
 			</section>
 		</section>
 	);
-}
-
-
-import React from 'react';
-import Intercom from '@intercom/messenger-js-sdk';
-
-export default function Component() {
-  Intercom({
-    app_id: 'wo0haiuj',
-    user_id: user.id, // IMPORTANT: Replace "user.id" with the variable you use to capture the user's ID
-    name: user.name, // IMPORTANT: Replace "user.name" with the variable you use to capture the user's name
-    email: user.email, // IMPORTANT: Replace "user.email" with the variable you use to capture the user's email
-    created_at: user.createdAt, // IMPORTANT: Replace "user.createdAt" with the variable you use to capture the user's sign-up date in a Unix timestamp (in seconds) e.g. 1704067200
-  });
-
-  return <div>Neon Host</div>;
 }
