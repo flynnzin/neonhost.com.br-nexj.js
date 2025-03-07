91daa3d3422fd3eb06817240c4791741d98080ba
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-07 02:03:01 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -156,23 +156,6 @@ export default function Home() {
 				}
 			`}</style> */}
 
-			<script>
-			(function(w,d,s,l,i){
-				w[l]=w[l]||[]; 
-				w[l].push({
-				'gtm.start': new Date().getTime(), 
-				event: 'gtm.js'
-				});
-				var f = d.getElementsByTagName(s)[0],
-					j = d.createElement(s),
-					dl = l != 'dataLayer' ? '&l=' + l : '';
-				j.async = true;
-				j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-				f.parentNode.insertBefore(j, f);
-			})(window, document, 'script', 'dataLayer', 'GTM-NLK4RWTG');
-			</script>
-
-
 			<section className="pt-[15%]">
 				<div className="container">
 					<div className="text-center">
