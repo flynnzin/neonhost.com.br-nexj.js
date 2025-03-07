a60d9fa8b0a4e49cd93ec15b55cdef793539221d
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-07 02:14:58 -0300

    Update _app.tsx

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
+import { AppProps } from 'next/app';
 const ReactGtm = require('react-gtm-module');
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     ReactGtm.initialize({ gtmId: 'GTM-NLK4RWTG' });
   }, []);
