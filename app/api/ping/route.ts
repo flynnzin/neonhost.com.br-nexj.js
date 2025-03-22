ec320dd127ccd699422c716d0d0acb91b7fa5aca
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-22 18:38:12 -0300

    fix: fixed ping

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -6,9 +6,11 @@ const execPromise = promisify(exec);
 
 export async function GET() {
 	try {
-		const { stdout } = await execPromise("ping -n 1 45.146.81.12");
-		const match = stdout.match(/tempo[=<](\d+)ms/);
-		const pingTime = match ? parseInt(match[1]) : null;
+		const { stdout } = await execPromise("ping -c 1 45.146.81.12");
+
+		// Regex para capturar "time=XXX ms"
+		const match = stdout.match(/time=(\d+(?:\.\d+)?)\s?ms/);
+		const pingTime = match ? parseFloat(match[1]) : null;
 
 		return NextResponse.json({ ping: pingTime });
 	} catch (error) {
