b2d45b2c8813d29420e928e6c1b6972492d3d32d
Cristopher Leonardo <Lzfts593@gmail.com>
2025-03-25 11:41:55 -0300

    Update card-products.tsx

--- app/_components/card-products.tsx
+++ app/_components/card-products.tsx
@@ -40,12 +40,10 @@ export function CardProduts(vpsItem: {
 						<span className="text-gray-400 text-sm font-normal ml-1">/mês</span>
 					</h1>
 					<p className="text-sm text-gray-400 flex items-center gap-1">
-						Recomendado para
-						<span className="rounded bg-gradient-to-r from-pink-400 to-purple-400 px-1 py-0.5 font-semibold text-white">
-							{vpsItem.description.jogadores ?? "Hospedado em São Paulo"}
-						</span>
+						{vpsItem.description.jogadores
+							? `Recomendado para ${vpsItem.description.jogadores} jogadores`
+							: "Hospedado em São Paulo"}
 					</p>
-
 				</div>
 
 				{/* Especificações Principais */}
